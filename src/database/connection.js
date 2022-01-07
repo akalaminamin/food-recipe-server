6f3c1c44b3453595ba9c128d394a015a86e24733
Al Amin <akalaminamin91@gmail.com>
2022-01-06 20:17:53 -0800

    added mongodb atlas database connction

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,17 @@
-const mongoose = require("mongoose"); 
-mongoose.connect("mongodb://localhost:27017/bookRecipe" 
-// {
-//     useCreateIndex:true,
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true
-// }
-).then(()=>{
-    console.log("Connection successful")
-}).catch((err) =>{
-    console.log("No  Connection")
-})
+const mongoose = require("mongoose");
+require('dotenv').config();
+const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6cenr.mongodb.net/recipe-book?retryWrites=true&w=majority`;
+// "mongodb://localhost:27017/bookRecipe"
+mongoose.connect(db
+    // {
+    //     useCreateIndex:true,
+    //     useNewUrlParser:true,
+    //     useUnifiedTopology:true
+    // }
+  )
+  .then(() => {
+    console.log("Connection successful");
+  })
+  .catch((err) => {
+    console.log("No  Connection");
+  });
--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const bookRecipe = require("../models/bookRecipeSchema");
 const {
   postFoodRecipe,
   getFoodRecipe,
